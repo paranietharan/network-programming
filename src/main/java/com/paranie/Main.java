2863bb66478ca44f869d37d746a5d49f8c4a161b
paranietharan <142080100+paranietharan@users.noreply.github.com>
2024-12-26 14:40:41 +0530

    test new Pc

--- src/main/java/com/paranie/Main.java
+++ src/main/java/com/paranie/Main.java
@@ -1,8 +1,9 @@
 package com.paranie;
 
-// check for email chaneg
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
+
+// Test new pc
